55400b5f470f7717850ac391e630986909a5208a
SebassG05 <152302090+SebassG05@users.noreply.github.com>
2025-05-25 18:12:35 +0200

    footer y arreglo

--- 24Seconds/src/app/historia/historia.component.ts
+++ 24Seconds/src/app/historia/historia.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-historia',
   standalone: true,
   templateUrl: './historia.component.html',
-  styleUrls: ['./historia.component.css']
+  styleUrls: ['./historia.component.css'],
+  imports: [
+    FooterComponent
+  ]
 })
 export class HistoriaComponent {}
