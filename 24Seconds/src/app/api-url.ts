650238c046252506df1c446d6231b9ac53c383d2
SebassG05 <152302090+SebassG05@users.noreply.github.com>
2025-06-03 22:16:54 +0200

    Commit arreglo fallo

--- 24Seconds/src/app/api-url.ts
+++ 24Seconds/src/app/api-url.ts
@@ -1,4 +1,4 @@
+import { environment } from '../environments/environment';
+
 // Centraliza la URL base de la API para todo el frontend
-export const API_URL = (typeof process !== 'undefined' && process.env && process.env['API_URL'])
-  ? process.env['API_URL']
-  : 'https://tfg-z7pz.onrender.com/api';
+export const API_URL = environment.API_URL;
---
+++ 24Seconds/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  API_URL: 'https://tfg-z7pz.onrender.com/api'
+};
